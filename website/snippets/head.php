885f29dc30d32db9e68565b0abca6c34ad87e8bb
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2021-02-02 12:29:46 +0100

    --added favicon

--- website/snippets/head.php
+++ website/snippets/head.php
@@ -9,6 +9,17 @@ echo "<html>
 	<meta name=\"keywords\" content=\"Watch Dogs 2, Data, modding\">
 	<meta name=\"author\" content=\"Nickwasused\">
 	<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">
+	<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/images/favicon/apple-touch-icon.png\">
+	<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/images/favicon/favicon-32x32.png\">
+	<link rel=\"icon\" type=\"image/png\" sizes=\"192x192\" href=\"/images/favicon/android-chrome-192x192.png\">
+	<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/images/favicon/favicon-16x16.png\">
+	<link rel=\"manifest\" href=\"/images/favicon/site.webmanifest\">
+	<link rel=\"mask-icon\" href=\"/images/favicon/safari-pinned-tab.svg\" color=\"#000000\">
+	<link rel=\"shortcut icon\" href=\"/images/favicon/favicon.ico\">
+	<meta name=\"msapplication-TileColor\" content=\"#da532c\">
+	<meta name=\"msapplication-TileImage\" content=\"/images/favicon/mstile-144x144.png\">
+	<meta name=\"msapplication-config\" content=\"/images/favicon/browserconfig.xml\">
+	<meta name=\"theme-color\" content=\"#ffffff\">
 	<link type='text/css' rel='stylesheet' href='/css/main.css'>
 </head>
 <a id=\"home\"></a>
