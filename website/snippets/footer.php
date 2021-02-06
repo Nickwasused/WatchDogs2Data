a328a2b038d515b7005f5a540651177e8e61c193
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2021-02-06 20:04:46 +0100

    --added footer

--- website/snippets/footer.php
+++ website/snippets/footer.php
@@ -4,6 +4,8 @@ $pdo = null;
 
 echo "
 <footer>
+	<center><p>Nickwasused ".date("Y")."<br>nickwasused.social@protonmail.com</p></center>
+
 	<!-- start image Lazy Loading -->
 	<script src=\"/scripts/lozad.js\"></script>
 	<script src=\"/scripts/picture.js\"></script>
