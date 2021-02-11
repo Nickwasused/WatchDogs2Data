836dd04769f33c5f5febdfd259a0cfd6245b5493
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2021-02-11 11:10:38 +0100

    Update constants.php

--- website/snippets/constants.php
+++ website/snippets/constants.php
@@ -9,10 +9,10 @@ $footer_email = "nickwasused.social@protonmail.com";
 
 if ($server_type = "dev") {
     ini_set('log_errors', 1);
-    $sanitizerstate = "sanitize_output";
+    $sanitizerstate = null;
 } else {
     ini_set('display_errors', 0);
-    $sanitizerstate = null;
+    $sanitizerstate = "sanitize_output";
 }
 
 ?>
